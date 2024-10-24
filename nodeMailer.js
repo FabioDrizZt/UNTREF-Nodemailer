bf705f9ad789abf25402c25f2d7848730fe8aee3
Fabio <Fabio@ada-school.com>
2024-10-24 20:31:23 -0300

    ejemplo con html

--- nodeMailer.js
+++ nodeMailer.js
@@ -10,18 +10,17 @@ const transporter = nodemailer.createTransport({
     user:process.env.EMAIL_USER,
     pass:process.env.EMAIL_PASSWORD
   }
-})
-
-// Paso 2: Definir las emailOptions
-const emailOptions = {
+}).sendMail({
   from: process.env.EMAIL_USER, // El correo del remitente
   to: 'drizzzt@gmail.com', // Email del destinatario
   subject: 'Probando nodemailer', // Asunto del correo
-  text: 'Funciona correctamente el envío de correos' // Cuerpo del mensaje
-}
-
-// Paso 3: Enviar el correo:
-transporter.sendMail(emailOptions, (err, info) => {
+  text: 'Funciona correctamente'
+/*   html: `
+  <h1 style="color: #4fc">Funciona correctamente el envío de correos</h1>
+  <p style="font-size: 20px; text-align: center">Este es un parrafo de prueba</p>
+  <img src="https://www.latercera.com/resizer/v2/6O3E6FX56FCYFGW5NZFOKYGNY4.jpg?quality=80&smart=true&auth=cb1335cb6bf0618491cd556ee65855efdd2ab2dae9eb059d2dadd30a41642898&width=690&height=502" alt="Imagen de pikachu" width="100%" />
+  ` */
+}, (err, info) => {
   if (err) {
     console.error('Ha ocurrido un ERROR al enviar el email: ', err);
   } else {
